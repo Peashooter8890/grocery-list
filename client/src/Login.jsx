ceef4cff09963a80cf84ccac4a959cb5035a3197
Eugene <kccctenshi@hotmail.com>
2023-06-06 16:51:51 -0400

    Login UI - w/o styling & logic

---
+++ client/src/Login.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+export const Login = () => {
+    const [username, setUsername] = useState('');
+    // const [pass, setPass] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(username);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            {/* <label for="username">User name</label> */}
+            <label>User name</label>
+            <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" placeholder="user name" name="username" id="username" />
+            <button type="submit">Log In</button>
+        </form>
+    )
+}
