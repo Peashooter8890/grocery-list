3d2be7a2ff2fcc4e049626fd0f99bc6d5174ace2
Eugene <kccctenshi@hotmail.com>
2023-06-30 16:42:22 -0400

    Login page styling

--- client/src/components/login-and-account/Login.jsx
+++ client/src/components/login-and-account/Login.jsx
@@ -3,6 +3,7 @@ import axiosInstance from '../../axiosConfig';
 import { useDispatch, useSelector } from 'react-redux';
 import { setLoginStatus } from '../../actions/AuthActions';
 import PopupWindow from "../utility/PopupWindow";
+import '../../App.css';
 
 const Login = () => {
     const dispatch = useDispatch();
@@ -58,7 +59,7 @@ const Login = () => {
     };
 
     return (
-        <div>
+        <div class="login">
             <PopupWindow 
                 title="FRESHLY BAKED NON-SHADY COOKIES. They are here. NOW." 
                 message="Our server baked some hot FRESHLY BAKED NON-SHADY COOKIES. Please enable our FRESHLY BAKED NON-SHADY COOKIES in your browser so that your browser can taste our FRESHLY BAKED NON-SHADY COOKIES. 
@@ -66,55 +67,76 @@ const Login = () => {
                 (such as the assumption that we are using shady cookies to enforce menace onto you, which we clearly aren't, because we don't know any buyers right now who would buy your personal information), please enable cookies so that we can spread our FRESHLY BAKED NON-SHADY COOKIES onto the world. Thank you."
             />
             <form onSubmit={handleLogin}>
-                <label>Email</label>
-                <input 
-                    value={loginEmail} 
-                    onChange={(e) => setLoginEmail(e.target.value)} 
-                    type="text" 
-                    placeholder="email" 
-                    name="email" 
-                    required
-                />
-                <label>Password</label>
-                <input 
-                    value={loginPassword} 
-                    onChange={(e) => setLoginPassword(e.target.value)} 
-                    type="password" 
-                    placeholder="password" 
-                    name="password" 
-                    required
-                />
-                <button type="submit">Log In</button>
+                <section>
+                    <label>Email</label>
+                    <input 
+                        value={loginEmail} 
+                        onChange={(e) => setLoginEmail(e.target.value)} 
+                        type="text" 
+                        placeholder="email" 
+                        name="email" 
+                        required
+                    />
+                </section>
+
+                <section>
+                    <label>Password</label>
+                    <input 
+                        value={loginPassword} 
+                        onChange={(e) => setLoginPassword(e.target.value)} 
+                        type="password" 
+                        placeholder="password" 
+                        name="password" 
+                        required
+                    />
+                </section>
+
+                <section>
+                    <button type="submit">Log In</button>
+                </section>
             </form>
-            <form onSubmit={handleSignUp}>
-                <label>Name</label>
-                <input 
-                    value={name} 
-                    onChange={(e) => setName(e.target.value)} 
-                    type="text" 
-                    placeholder="name" 
-                    name="name" 
-                    required
-                />
-                <input 
-                    value={signupEmail} 
-                    onChange={(e) => setSignupEmail(e.target.value)} 
-                    type="text" 
-                    placeholder="email" 
-                    name="email" 
-                    required
-                />
-                <label>Password</label>
-                <input 
-                    value={signupPassword} 
-                    onChange={(e) => setSignupPassword(e.target.value)} 
-                    type="password" 
-                    placeholder="password" 
-                    name="password" 
-                    required
-                />
-                <button type="submit">Sign Up</button>
+            <form class="register" onSubmit={handleSignUp}>
+                <section>
+                    <label>Name</label>
+                    <input 
+                        value={name} 
+                        onChange={(e) => setName(e.target.value)} 
+                        type="text" 
+                        placeholder="name" 
+                        name="name" 
+                        required
+                    />
+                </section>
+                
+                <section>
+                    <label>Email</label>
+                    <input 
+                        value={signupEmail} 
+                        onChange={(e) => setSignupEmail(e.target.value)} 
+                        type="text" 
+                        placeholder="email" 
+                        name="email" 
+                        required
+                    />
+                </section>
+                
+                <section>
+                    <label>Password</label>
+                    <input 
+                        value={signupPassword} 
+                        onChange={(e) => setSignupPassword(e.target.value)} 
+                        type="password" 
+                        placeholder="password" 
+                        name="password" 
+                        required
+                    />
+                </section>
+                
+                <section>
+                    <button type="submit">Sign Up</button>
+                </section>
             </form>
+
             <span style={{color: "green"}}>{successMessage && <div className="successMessage">{successMessage}</div>}</span>
             <span style={{color: "red"}}>{errorMessage && <div className="errorMessage">{errorMessage}</div>}</span>
             {isLoggedIn 
