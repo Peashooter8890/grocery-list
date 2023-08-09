e26b6b6e31b59c6bacb15ea176e623d5142a5f00
Jin <jroynam@gmail.com>
2023-08-09 15:09:47 -0400

    implemented sign up page

--- client/src/components/login-and-account/SignUp.jsx
+++ client/src/components/login-and-account/SignUp.jsx
@@ -7,7 +7,7 @@ import ReverseAuthProtector from "../utility/ReverseAuthProtector";
 import '../../App.css';
 import { useNavigate } from "react-router-dom";
 
-const Login = () => {
+const SignUp = () => {
     const dispatch = useDispatch();
     const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
     const [username, setUsername] = useState('');
@@ -60,39 +60,51 @@ const Login = () => {
     return (
         <div className="flex flex-col justify-between items-center h-screen">
             <CookieWarningWindow/>
-            <form className="flex flex-col gap-1.5 justify-center items-end border-2 border-gray-900 w-72 h-60" onSubmit={handleLogin}>
+            <form className="flex flex-col gap-1.5 justify-center items-end border-2 border-gray-900 w-72 h-60" onSubmit={handleSignUp}>
+                <section className="gap-1.5 flex justify-right items-center pr-2">
+                    <label>Name</label>
+                    <input 
+                        className="h-8 w-48"
+                        value={username} 
+                        onChange={(e) => setUsername(e.target.value)} 
+                        type="text" 
+                        placeholder="name" 
+                        name="name" 
+                        required
+                    />
+                </section>
+                
                 <section className="gap-1.5 flex justify-right items-center pr-2">
                     <label>Email</label>
                     <input 
                         className="h-8 w-48"
-                        value={loginEmail} 
-                        onChange={(e) => setLoginEmail(e.target.value)} 
+                        value={signupEmail} 
+                        onChange={(e) => setSignupEmail(e.target.value)} 
                         type="text" 
                         placeholder="email" 
                         name="email" 
                         required
                     />
                 </section>
-
+                
                 <section className="gap-1.5 flex justify-right items-center pr-2">
                     <label>Password</label>
                     <input 
                         className="h-8 w-48"
-                        value={loginPassword} 
-                        onChange={(e) => setLoginPassword(e.target.value)} 
+                        value={signupPassword} 
+                        onChange={(e) => setSignupPassword(e.target.value)} 
                         type="password" 
                         placeholder="password" 
                         name="password" 
                         required
                     />
                 </section>
-
                 <section className="w-[12.5rem]">
-                    <button className="h-8 w-20 border-2 border-gray-200 bg-white hover:bg-gray-200 text-black" type="submit">Log In</button>
+                    <button className="h-8 w-20 border-2 border-gray-200 bg-white hover:bg-gray-200 text-black" type="submit">Sign Up</button>
                 </section>
             </form>
-           
-            <button onClick={() => {navigate("/signup")}}>Need to create an account? Sign up!</button>
+
+            <button onClick={() => {navigate("/login")}}>Already created an account? Log in!</button>
 
             {isLoggedIn 
                 ? 
@@ -105,4 +117,7 @@ const Login = () => {
     );
 };
 
-export default ReverseAuthProtector(Login);
+export default ReverseAuthProtector(SignUp);
+
+
+
